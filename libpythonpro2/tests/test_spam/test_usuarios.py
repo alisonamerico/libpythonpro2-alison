6f02a43c34def6b7f93d6badab54f7c3887dc064
alisonamerico <alison.americo@gmail.com>
2020-04-25 13:29:39 -0300

    criado producao de codigo testavel

--- libpythonpro2/tests/test_spam/test_usuarios.py
+++ libpythonpro2/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro2.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Alison')
+    usuario = Usuario(nome='Alison', email='alison@python.pro.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Alison'), Usuario(nome='Luciano')]
+    usuarios = [
+        Usuario(nome='Alison', email='alison@python.pro.br'),
+        Usuario(nome='Luciano', email='luciano@python.pro.br')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
