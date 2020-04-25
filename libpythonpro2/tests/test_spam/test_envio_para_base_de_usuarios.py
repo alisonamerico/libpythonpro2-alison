cf83014de248fe18f9ee82535d3d2e4d87fb291b
alisonamerico <alison.americo@gmail.com>
2020-04-25 13:29:39 -0300

    criado producao de codigo testavel

--- libpythonpro2/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro2/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,31 @@
+
+import pytest
+
 from libpythonpro2.spam.enviador_de_email import Enviador
 from libpythonpro2.spam.main import EnviadorDeSpam
+from libpythonpro2.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Alison', email='alison@python.pro.br'),
+            Usuario(nome='Luciano', email='luciano@python.pro.br')
+        ],
+        [
+            Usuario(nome='Alison', email='alison@python.pro.br')
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'alison@python.pro.br',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
