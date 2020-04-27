a9e403d1d3184a988f6237fefce85364a6d32be4
alisonamerico <alison.americo@gmail.com>
2020-04-27 17:58:41 -0300

    corrigido pull request

--- libpythonpro2/tests/test_spam/test_github_api.py
+++ libpythonpro2/tests/test_spam/test_github_api.py
@@ -1,12 +1,26 @@
-from libpythonpro2 import github_api
+import pytest
 from unittest.mock import Mock
+from libpythonpro2 import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatat_url():
     resp_mock = Mock()
+    url = 'https://avatars0.githubusercontent.com/u/13425415?v=4'
     resp_mock.json.return_value = {
-        'avatar_url': 'https://avatars0.githubusercontent.com/u/13425415?v=4'
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatat_url):
+    url = github_api.buscar_avatar('alisonamerico')
+    assert avatat_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('alisonamerico')
     assert 'https://avatars0.githubusercontent.com/u/13425415?v=4' == url
