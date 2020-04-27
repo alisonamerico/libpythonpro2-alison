b23de2ffaae3e72e41f261ca11a6e31d80f682d2
alisonamerico <alison.americo@gmail.com>
2020-04-27 12:44:54 -0300

    Criada injecao de dependencias

--- libpythonpro2/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro2/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -21,7 +21,7 @@ from libpythonpro2.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'alison@python.pro.br',
@@ -29,3 +29,32 @@ def test_qtd_de_spam(sessao, usuarios):
         'Confira os módulos fantásticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Alison', email='alison@python.pro.br')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'luciano@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'luciano@python.pro.br',
+        'alison@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
