e6d968100ebc1223a0ad7256ff460b69b238467f
alisonamerico <alison.americo@gmail.com>
2020-04-28 14:51:21 -0300

    usando o pytest-mocker

--- libpythonpro2/tests/test_spam/test_github_api.py
+++ libpythonpro2/tests/test_spam/test_github_api.py
@@ -1,19 +1,18 @@
-import pytest
-from unittest.mock import Mock
 from libpythonpro2 import github_api
+from unittest.mock import Mock
+import pytest
 
 
 @pytest.fixture
-def avatat_url():
+def avatat_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars0.githubusercontent.com/u/13425415?v=4'
     resp_mock.json.return_value = {
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro2.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatat_url):
